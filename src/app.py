0f4a2bb377348df399ae70fb17c76d44f80d2a17
dianamonroe <dianamonroe79@gmail.com>
2025-01-26 18:46:35 +0000

    test! git commit -m test

--- src/app.py
+++ src/app.py
@@ -1,4 +1,24 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+from ultralytics import YOLO
+from PIL import Image
+import io
 
-# your code here
+app = Flask(__name__)
+model = YOLO("Yolov86thRoundbestWeights.pt")  # Modelo entrenado
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        file = request.files["image"]
+        if file:
+            image = Image.open(file.stream)
+            results = model.predict(image, conf=0.25)
+            label = results[0].names[results[0].boxes.cls[0].item()]
+            confidence = results[0].boxes.conf[0].item() * 100
+            return render_template(
+                "index.html", label=label, confidence=confidence, image=file.filename
+            )
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
